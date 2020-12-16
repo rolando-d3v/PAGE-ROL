fab7d4445962fe331087dce0f382a28e91854c04
raidentek <33987212+raidentek@users.noreply.github.com>
2020-12-16 15:26:43 -0500

    nuevo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
